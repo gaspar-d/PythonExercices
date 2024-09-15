8491dfcc3b887ee7d077d672201614069fe06f28
Diogo Gaspar <diogo.gaspar@me.com>
2024-09-15 13:25:00 -0300

    feat: show balance with timestamps
    improved CLI readability with colors
    changed lists to dict in order to save dates and transactions together

--- Challenge01_BankAccount.py
+++ Challenge01_BankAccount.py
@@ -1,56 +1,87 @@
-def do_something():
-    print("something")
-
+import datetime as dt
+from typing import Dict, Union
 
 balance = 0
-deposits = []
-withdraws = []
-DAILY_WITHDRAWAL_LIMIT = 3
+counter = 0
+DAILY_TRANSACTION_LIMIT = 10
+transactions: Dict[int, Dict[str, Union[str, float | dt.datetime]]] = {}
 
 
-def deposit_handler(balance):
+def transactions_record(option: str, amount: float, date: dt.datetime) -> bool:
+    global counter
+    counter += 1
+    today = dt.date.today()
+    if option.lower() == "d":
+        if today and counter > DAILY_TRANSACTION_LIMIT:
+            print("\033[31mExceeded daily transaction limit of 10\033[0m")
+            return False
+        else:
+            transactions[counter] = {"amount": amount, "date": date}
+            return True
+    elif option.lower() == "w":
+        if today and counter > DAILY_TRANSACTION_LIMIT:
+            print("\033[31mExceeded daily transaction limit of 10\033[0m")
+            return False
+        else:
+            transactions[counter] = {"amount": -(amount), "date": date}
+            return True
+    else:
+        return False
+
+
+def deposit_handler(balance: float) -> float:
     try:
         deposit = float(input("Enter the amount to deposit: "))
         if deposit < 1:
-            print("Deposit must be greater than 0")
+            print("\033[33mDeposit must be greater than 0\033[0m")
         else:
-            deposits.append(deposit)
-            balance += deposit
-            print(f"Deposited: R$ {deposit}")
+            date = dt.datetime.now()
+            if transactions_record("d", deposit, date):
+                balance += deposit
+                print(f"Deposited: R$ \033[32m{deposit}\033[0m")
         return balance
     except ValueError:
-        print("Deposit must be a number")
+        print("\033[33mDeposit must be a number\033[0m")
         return balance
 
 
-def withdraw_handler(balance):
+def withdraw_handler(balance: float) -> float:
     try:
         withdraw = float(input("Enter the amount to withdraw: "))
         if withdraw < 1:
-            print("Withdraw must be greater than 0")
+            print("\033[33mWithdraw must be greater than 0\033[0m")
         else:
-            withdraws.append(-(withdraw))
             if withdraw > 500:
-                print("Withdraw limit exceeded")
+                print("\033[32mWithdraw limit exceeded\033[0m")
             elif withdraw > balance:
-                print("Balance is insufficient")
-            elif len(withdraws) > DAILY_WITHDRAWAL_LIMIT:
-                print("Exceeded daily withdrawal limit of 3")
+                print("\033[32mBalance is insufficient\033[0m")
             else:
-                balance -= withdraw
-                print(f"Withdrawn: R$ {withdraw}")
+                date = dt.datetime.now()
+                if transactions_record("w", withdraw, date):
+                    balance -= withdraw
+                    print(f"Withdrawn: R$ \033[31m{withdraw}\033[0m")
         return balance
     except ValueError:
-        print("Withdraw must be a number")
+        print("\033[33mWithdraw must be a number\033[0m")
         return balance
 
 
-def balance_handler(balance):
-    formatted_deposits = [f"R$ {deposit:.2f}" for deposit in deposits]
-    formatted_withdraws = [f"R$ {withdraw:.2f}" for withdraw in withdraws]
-    print(
-        f"Deposits: {formatted_deposits} \nWithdraws: {formatted_withdraws} \nBalance: R$ {balance:.2f}\n"
-    )
+def balance_handler(balance: float) -> float:
+    for transaction in transactions.values():
+        amount = transaction["amount"]
+        date = transaction["date"]
+        if isinstance(amount, float) and amount > 0:
+            if isinstance(date, dt.datetime):
+                print(
+                    f"R$ \033[92m{transaction['amount']:10.2f}\033[0m : {date.strftime('%d/%m/%Y %H:%M:%S')}"
+                )
+        elif isinstance(amount, float) and amount < 0:
+            if isinstance(date, dt.datetime):
+                print(
+                    f"R$ \033[91m{transaction['amount']:10.2f}\033[0m : {date.strftime('%d/%m/%Y %H:%M:%S')}"
+                )
+
+    print(f"Balance: R$ \033[94m{balance:.2f}\033[0m\n")
     return balance
 
 
@@ -67,7 +98,7 @@ while True:
         balance = balance_handler(balance)
 
     elif option.lower() == "q":
-        print("Service terminated. \nHave a nice day.")
+        print("\33[96mService terminated. \nHave a nice day.\33[0m")
         break
     else:
-        print("invalid option")
+        print("\033[31m!!!Invalid Option!!!\033[0m")
--- README.md
+++ README.md
@@ -20,17 +20,17 @@ Just one user, no account, no login
 - [x] The balance is stored in the variable balance.
 - [x] The balance can never be negative.
 - [x] If the balance is not enough to complete the transaction, print "Saldo Insuficiente".
-- [ ] balance option should list all deposits made
-- [ ] balance option should list all withdrawals made
-- [ ] balance option should list total balance
-- [ ] if balance is zero, print "Não foram realizadas movimentações"
+- [x] balance option should list all deposits made
+- [x] balance option should list all withdrawals made
+- [x] balance option should list total balance
+- [x] if balance is zero, print "Não foram realizadas movimentações"
 - [x] format should be: R$ xxx.xx
 
 # Challenge 2: Timestamps
 ## Date: 2022-10-26
 
 ### Transactions Limit
-- [] Transactions limit per day should be 10
-- [] An Alert if user has exceeded transactions limit
-- [] Balance must show the timestamp (date and hour) of the all transactions
+- [x] Transactions limit per day should be 10
+- [x] An Alert if user has exceeded transactions limit
+- [x] Balance must show the timestamp (date and hour) of the all transactions
 
